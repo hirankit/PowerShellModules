1ed289a156cd331c13281ca23280c544bd8077eb
Hirankit Tantirapan <hirankit@tantirapan.com>
2024-09-22 14:28:17 -0700

    fix: dot-sourcing line Install-WinGet

--- Scripts/Install-WinGet.ps1
+++ Scripts/Install-WinGet.ps1
@@ -1,4 +1,10 @@
-. ../Functions/Add-WebAppxPackage.ps1
+# Get the directory of the current script
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# Construct the relative path
+$pathToFunctions = Join-Path -Path $scriptDir -ChildPath "../Functions/"
+
+. (Join-Path $pathToFunctions -ChildPath "Add-WebAppxPackage.ps1")
 
 $WingetAppPackageUrl = 'https://aka.ms/getwinget'
 
